ff8193eb84ede066f759b27d8f7546b24138bafa
manyyeon <udayeon@naver.com>
2025-05-18 10:55:54 -0600

    Update site configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://manyyeon-blog.vercel.app/",
+	site: "https://manyyeon.vercel.app/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
